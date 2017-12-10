3b671ce8eabf1081e6d12137b77ece6110ec1762
it-tommy <it.java.tommy@gmail.com>
2017-12-10 16:28:01 -0600

    changed message - 12/10/2017

--- src/main/java/com/sss/ServletTest.java
+++ src/main/java/com/sss/ServletTest.java
@@ -1,6 +1,8 @@
 package com.sss;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,9 @@ public class ServletTest extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Hello World!");
 	}
 
 
